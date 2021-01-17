2c4136351ffc1cb97cb7eedf36461edf770095ab
new-to-coding <54109171+new-to-coding@users.noreply.github.com>
2021-01-17 15:20:38 +0900

    register serviceworker

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,8 @@ import AWSAppSyncClient, { AUTH_TYPE } from "aws-appsync";
 import AppSyncConfig from "./aws-exports";
 import { listPosts } from "./graphql/queries";
 import Amplify, { Auth } from "aws-amplify";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 import { ApolloProvider } from "react-apollo";
 
 //import { withAuthenticator } from 'aws-amplify-react';
@@ -47,3 +49,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
