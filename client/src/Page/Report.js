cacac50612cdc492ea36db86d086bd61786a4e75
steven <steven@kaist.ac.kr>
2021-01-27 23:52:53 +0900

    loading

--- client/src/Page/Report.js
+++ client/src/Page/Report.js
@@ -5,6 +5,7 @@ import "../App.css";
 import gql from "graphql-tag";
 
 import { serviceByUser } from "../graphql/queries";
+import PullToRefresh from "react-simple-pull-to-refresh";
 
 import ToolBar from "../components/ToolBar";
 import Main from "../components/MainReport";
@@ -110,292 +111,292 @@ export function Report(props) {
   };
 
   return (
-    <div style={{ width: "100%", alignContent: "center", height: "100vh" }}>
-      {loadingLayout}
-
-      <div>
-        <div
-          position="absolute"
-          className={clsx(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-          style={{
-            width: "100%",
-            height: "5.73vh",
-          }}
-        >
-          <div style={{ width: "100%" }}>
-            <Box
-              display="flex"
-              // p={1}
-              alignItems="center"
-              style={{
-                margin: 0,
-                padding: "4px 16px 4px 16px",
-                height: "5.72vh",
-              }}
-            >
-              <Box p={1} flexGrow={1} style={{ margin: 0, padding: 0 }}>
-                <Button
-                  onClick={handleDrawerOpen}
-                  style={{
-                    padding: 0,
-                    margin: 0,
-                    minHeight: 0,
-                    minWidth: 0,
-                    borderRadius: "100%",
-                  }}
-                >
-                  <img
-                    alt="name"
-                    src="ProfileIcon.png"
-                    style={{ width: "4.16vh", height: "4.16vh" }}
-                  ></img>
-                </Button>
-              </Box>
-
+    <PullToRefresh refreshingContent={loadingLayout} onRefresh={this.onRefresh}>
+      <div style={{ width: "100%", alignContent: "center", height: "100vh" }}>
+        <div>
+          <div
+            position="absolute"
+            className={clsx(classes.appBar, {
+              [classes.appBarShift]: open,
+            })}
+            style={{
+              width: "100%",
+              height: "5.73vh",
+            }}
+          >
+            <div style={{ width: "100%" }}>
               <Box
-                p={1}
+                display="flex"
+                // p={1}
+                alignItems="center"
                 style={{
                   margin: 0,
-                  padding: 0,
+                  padding: "4px 16px 4px 16px",
+                  height: "5.72vh",
                 }}
               >
-                <Button
-                  component={Link}
-                  to="/Notification"
+                <Box p={1} flexGrow={1} style={{ margin: 0, padding: 0 }}>
+                  <Button
+                    onClick={handleDrawerOpen}
+                    style={{
+                      padding: 0,
+                      margin: 0,
+                      minHeight: 0,
+                      minWidth: 0,
+                      borderRadius: "100%",
+                    }}
+                  >
+                    <img
+                      alt="name"
+                      src="ProfileIcon.png"
+                      style={{ width: "4.16vh", height: "4.16vh" }}
+                    ></img>
+                  </Button>
+                </Box>
+
+                <Box
+                  p={1}
                   style={{
-                    padding: 0,
                     margin: 0,
-                    minHeight: 0,
-                    minWidth: 0,
-                    borderRadius: "100%",
+                    padding: 0,
                   }}
                 >
-                  <img
-                    src="/Icons[32]/Type=Notifications.svg"
-                    alt="name"
-                    style={{ width: "4.16vh", height: "4.16vh" }}
-                  ></img>
-                </Button>
+                  <Button
+                    component={Link}
+                    to="/Notification"
+                    style={{
+                      padding: 0,
+                      margin: 0,
+                      minHeight: 0,
+                      minWidth: 0,
+                      borderRadius: "100%",
+                    }}
+                  >
+                    <img
+                      src="/Icons[32]/Type=Notifications.svg"
+                      alt="name"
+                      style={{ width: "4.16vh", height: "4.16vh" }}
+                    ></img>
+                  </Button>
+                </Box>
               </Box>
-            </Box>
+            </div>
+          </div>
+          <div
+            position="fixed"
+            className={clsx(classes.appBar, {
+              [classes.appBarShift]: open,
+            })}
+            style={{
+              width: "100%",
+              height: "5.73vh",
+              marginTop: "1.56vh",
+            }}
+          >
+            <ToolBar value={ind} setValue={setIndex} />
+          </div>
+
+          <div
+            position="fixed"
+            className={clsx(classes.appBar, {
+              [classes.appBarShift]: open,
+            })}
+            style={{
+              width: "100%",
+              alignContent: "center",
+            }}
+          >
+            {(() => {
+              if (ind == 0) {
+                return <Main list={data} />;
+              } else {
+                return <Main2 list={data} />;
+              }
+            })()}
+          </div>
+
+          <div
+            position="fixed"
+            className={clsx(classes.appBar, {
+              [classes.appBarShift]: open,
+            })}
+          >
+            <BottomNavigation value={value} setValue={setValue} />
           </div>
         </div>
-        <div
-          position="fixed"
-          className={clsx(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-          style={{
-            width: "100%",
-            height: "5.73vh",
-            marginTop: "1.56vh",
+        <Drawer
+          className={classes.drawer}
+          anchor="left"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
           }}
+          onClick={toggleDrawer("left", false)}
         >
-          <ToolBar value={ind} setValue={setIndex} />
-        </div>
+          <div
+            style={{
+              marginTop: 16,
+              padding: "12px 20px 12px 20px",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <img
+              alt="name"
+              src="ProfileIcon.png"
+              style={{ width: "4.16vh", height: "4.16vh" }}
+            ></img>
+            <Typography
+              style={{
+                margin: 0,
+                padding: 0,
+                marginLeft: 16,
+                fontWeight: 500,
+                fontSize: 17,
+                alignItems: "center",
+              }}
+            >
+              {props.userData.name}
+            </Typography>
+          </div>
+          {/* Notice List */}
+          <Button
+            component={Link}
+            to="/Profile"
+            style={{
+              margin: 0,
+              padding: 0,
 
-        <div
-          position="fixed"
-          className={clsx(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-          style={{
-            width: "100%",
-            alignContent: "center",
-          }}
-        >
-          {(() => {
-            if (ind == 0) {
-              return <Main list={data} />;
-            } else {
-              return <Main2 list={data} />;
-            }
-          })()}
-        </div>
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "flex-start",
+              marginTop: 16,
+              height: 48,
+              paddingLeft: 20,
+              paddingRight: 20,
+              paddingTop: 12,
+              paddingBottom: 12,
+              textTransform: "none",
+            }}
+          >
+            <img
+              alt="Notice"
+              src="/Icons[24]/Type=Profile.svg"
+              style={{ width: "3.125vh", height: "3.125vh" }}
+            ></img>
 
-        <div
-          position="fixed"
-          className={clsx(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-        >
-          <BottomNavigation value={value} setValue={setValue} />
-        </div>
+            <Typography
+              style={{
+                margin: 0,
+                padding: 0,
+                marginLeft: 16,
+                fontWeight: 500,
+                fontSize: 17,
+                alignItems: "center",
+              }}
+            >
+              Profile
+            </Typography>
+          </Button>
+          <Button
+            component={Link}
+            to="/Profile"
+            style={{
+              margin: 0,
+              padding: 0,
+
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "flex-start",
+              marginTop: 16,
+              paddingLeft: 20,
+              paddingRight: 20,
+              paddingTop: 12,
+              paddingBottom: 12,
+              height: 48,
+              textTransform: "none",
+            }}
+          >
+            <img
+              alt="name"
+              src="/Icons[24]/Type=Notice.svg"
+              style={{ width: "3.125vh", height: "3.125vh" }}
+            ></img>
+            <Typography
+              style={{
+                margin: 0,
+                padding: 0,
+                marginLeft: 16,
+                fontWeight: 500,
+                fontSize: 17,
+                alignItems: "center",
+              }}
+            >
+              Notice Board
+            </Typography>
+          </Button>
+
+          <div
+            style={{ backgroundColor: "grey", height: "0.5px", marginTop: 16 }}
+          ></div>
+          {/* Privacy and Settings */}
+          <Box
+            Button
+            style={{
+              marginTop: 16,
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              height: 48,
+            }}
+          >
+            <Typography
+              style={{
+                margin: 0,
+                padding: 0,
+                paddingTop: 12,
+                paddingLeft: 20,
+                paddingBottom: 12,
+                fontWeight: 500,
+                fontSize: 17,
+                alignItems: "center",
+              }}
+            >
+              Privacy and Settings
+            </Typography>
+          </Box>
+          {/* Customer Support */}
+          <Box
+            Button
+            style={{
+              marginTop: 16,
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              height: 48,
+            }}
+          >
+            <Typography
+              style={{
+                margin: 0,
+                padding: 0,
+                paddingTop: 12,
+                paddingLeft: 20,
+                paddingBottom: 12,
+                fontWeight: 500,
+                fontSize: 17,
+                alignItems: "center",
+              }}
+            >
+              Customer Support
+            </Typography>
+          </Box>
+        </Drawer>
       </div>
-      <Drawer
-        className={classes.drawer}
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        onClick={toggleDrawer("left", false)}
-      >
-        <div
-          style={{
-            marginTop: 16,
-            padding: "12px 20px 12px 20px",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <img
-            alt="name"
-            src="ProfileIcon.png"
-            style={{ width: "4.16vh", height: "4.16vh" }}
-          ></img>
-          <Typography
-            style={{
-              margin: 0,
-              padding: 0,
-              marginLeft: 16,
-              fontWeight: 500,
-              fontSize: 17,
-              alignItems: "center",
-            }}
-          >
-            {props.userData.name}
-          </Typography>
-        </div>
-        {/* Notice List */}
-        <Button
-          component={Link}
-          to="/Profile"
-          style={{
-            margin: 0,
-            padding: 0,
-
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "flex-start",
-            marginTop: 16,
-            height: 48,
-            paddingLeft: 20,
-            paddingRight: 20,
-            paddingTop: 12,
-            paddingBottom: 12,
-            textTransform: "none",
-          }}
-        >
-          <img
-            alt="Notice"
-            src="/Icons[24]/Type=Profile.svg"
-            style={{ width: "3.125vh", height: "3.125vh" }}
-          ></img>
-
-          <Typography
-            style={{
-              margin: 0,
-              padding: 0,
-              marginLeft: 16,
-              fontWeight: 500,
-              fontSize: 17,
-              alignItems: "center",
-            }}
-          >
-            Profile
-          </Typography>
-        </Button>
-        <Button
-          component={Link}
-          to="/Profile"
-          style={{
-            margin: 0,
-            padding: 0,
-
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "flex-start",
-            marginTop: 16,
-            paddingLeft: 20,
-            paddingRight: 20,
-            paddingTop: 12,
-            paddingBottom: 12,
-            height: 48,
-            textTransform: "none",
-          }}
-        >
-          <img
-            alt="name"
-            src="/Icons[24]/Type=Notice.svg"
-            style={{ width: "3.125vh", height: "3.125vh" }}
-          ></img>
-          <Typography
-            style={{
-              margin: 0,
-              padding: 0,
-              marginLeft: 16,
-              fontWeight: 500,
-              fontSize: 17,
-              alignItems: "center",
-            }}
-          >
-            Notice Board
-          </Typography>
-        </Button>
-
-        <div
-          style={{ backgroundColor: "grey", height: "0.5px", marginTop: 16 }}
-        ></div>
-        {/* Privacy and Settings */}
-        <Box
-          Button
-          style={{
-            marginTop: 16,
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            height: 48,
-          }}
-        >
-          <Typography
-            style={{
-              margin: 0,
-              padding: 0,
-              paddingTop: 12,
-              paddingLeft: 20,
-              paddingBottom: 12,
-              fontWeight: 500,
-              fontSize: 17,
-              alignItems: "center",
-            }}
-          >
-            Privacy and Settings
-          </Typography>
-        </Box>
-        {/* Customer Support */}
-        <Box
-          Button
-          style={{
-            marginTop: 16,
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            height: 48,
-          }}
-        >
-          <Typography
-            style={{
-              margin: 0,
-              padding: 0,
-              paddingTop: 12,
-              paddingLeft: 20,
-              paddingBottom: 12,
-              fontWeight: 500,
-              fontSize: 17,
-              alignItems: "center",
-            }}
-          >
-            Customer Support
-          </Typography>
-        </Box>
-      </Drawer>
-    </div>
+    </PullToRefresh>
   );
 }
 
