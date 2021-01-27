4698e28518b06135857d3f0fbd46a29c32932075
youngmi97 <jeong7331@kaist.ac.kr>
2021-01-27 19:00:55 +0900

    transaction plaid

--- client/src/components/PlaidLink.js
+++ client/src/components/PlaidLink.js
@@ -48,28 +48,24 @@ class PlaidLogin extends Component {
     this.handleOnExit = this.handleOnExit.bind(this);
   }
 
+  //axios base url : https://j99vqavepi.execute-api.us-east-2.amazonaws.com/dev
+  //axios local base url : https://localhost:5000
+
   handleOnSuccess(public_token, metadata) {
     axios
-      .post(
-        "https://j99vqavepi.execute-api.us-east-2.amazonaws.com/dev/auth/public_token",
-        {
-          public_token: public_token,
-        }
-      )
+      .post("http://localhost:5000/auth/public_token", {
+        public_token: public_token,
+      })
       .then((response) =>
-        axios
-          .get(
-            "https://j99vqavepi.execute-api.us-east-2.amazonaws.com/dev/transactions"
-          )
-          .then((res) => {
-            this.setState({ transactions: res.data.transactions.transactions });
-            this.setState({ accounts: res.data.transactions.accounts });
+        axios.get("http://localhost:5000/transactions").then((res) => {
+          this.setState({ transactions: res.data.transactions.transactions });
+          this.setState({ accounts: res.data.transactions.accounts });
 
-            //this.props.history.push("/Accounts");
-            console.log("accountsCount", this.state.accounts.length);
-            //console.log("accounts", res.data.transactions.accounts);
-            console.log("transactionsCount", this.state.transactions.length);
-          })
+          //this.props.history.push("/Accounts");
+          console.log("accountsCount", this.state.accounts.length);
+          console.log("accounts", this.state.accounts);
+          console.log("transactionsCount", this.state.transactions.length);
+        })
       );
     console.log("handleOnSuccess");
   }
