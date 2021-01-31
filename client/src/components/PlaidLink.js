3ab8b27715eac0b6289dc9cb64a28c27c3a3e059
youngmi97 <jeong7331@kaist.ac.kr>
2021-01-31 15:22:06 +0900

    add endpoint

--- client/src/components/PlaidLink.js
+++ client/src/components/PlaidLink.js
@@ -56,20 +56,27 @@ class PlaidLogin extends Component {
 
   handleOnSuccess(public_token, metadata) {
     axios
-      .post("http://localhost:5000/auth/public_token", {
-        public_token: public_token,
-        userData: this.props.userData.sub,
-      })
+      .post(
+        "https://j99vqavepi.execute-api.us-east-2.amazonaws.com/dev/public_token",
+        {
+          public_token: public_token,
+          userData: this.props.userData.sub,
+        }
+      )
       .then((response) =>
-        axios.get("http://localhost:5000/transactions").then((res) => {
-          this.setState({ transactions: res.data.transactions.transactions });
-          this.setState({ accounts: res.data.transactions.accounts });
+        axios
+          .get(
+            "https://j99vqavepi.execute-api.us-east-2.amazonaws.com/dev/transactions"
+          )
+          .then((res) => {
+            this.setState({ transactions: res.data.transactions.transactions });
+            this.setState({ accounts: res.data.transactions.accounts });
 
-          //this.props.history.push("/Accounts");
-          console.log("accountsCount", this.state.accounts.length);
-          console.log("accounts", this.state.accounts);
-          console.log("transactionsCount", this.state.transactions.length);
-        })
+            //this.props.history.push("/Accounts");
+            console.log("accountsCount", this.state.accounts.length);
+            console.log("accounts", this.state.accounts);
+            console.log("transactionsCount", this.state.transactions.length);
+          })
       );
     console.log("handleOnSuccess");
   }
