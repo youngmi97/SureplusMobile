5289ce8d29469fdae8bbaf396626a6683fb5cfd8
youngmi97 <jeong7331@kaist.ac.kr>
2021-03-01 18:07:21 +0900

    modify PlaidLink axios

--- client/src/components/PlaidLinkFirst.js
+++ client/src/components/PlaidLinkFirst.js
@@ -62,42 +62,30 @@ class PlaidLogin extends Component {
 
   handleOnSuccess(public_token, metadata) {
     this.props.setState();
-    axios
-      .post(
-        "https://j99vqavepi.execute-api.us-east-2.amazonaws.com/dev/auth/public_token",
-        {
-          public_token: public_token,
-          userData: this.props.userData.sub,
-        }
-      )
-      .then(async (response) => {
-        console.log("access token", response.data.access_token);
+    API.post("plaidhandler", "/auth/publictoken", {
+      public_token: public_token,
+      userData: this.props.userData.sub,
+    }).then(async (response) => {
+      const plaidTokenUpdated = await API.graphql(
+        graphqlOperation(updateUser, {
+          input: {
+            id: this.props.userData.sub,
+            plaidToken: response.data.access_token,
+          },
+        })
+      );
 
-        const plaidTokenUpdated = await API.graphql(
-          graphqlOperation(updateUser, {
-            input: {
-              id: this.props.userData.sub,
-              plaidToken: response.data.access_token,
-            },
-          })
-        );
-        console.log("plaidTokenUpdated", plaidTokenUpdated);
+      console.log("plaidTokenUpdated", plaidTokenUpdated);
 
-        //API.graphql(graphqlOperation(createTodo, { input: todo }));
+      API.get("plaidhandler", "/transactions", {}).then((res) => {
+        this.setState({ transactions: res.data.transactions.transactions });
+        this.setState({ accounts: res.data.transactions.accounts });
 
-        axios
-          .get(
-            "https://j99vqavepi.execute-api.us-east-2.amazonaws.com/dev/transactions"
-          )
-          .then((res) => {
-            this.setState({ transactions: res.data.transactions.transactions });
-            this.setState({ accounts: res.data.transactions.accounts });
-
-            console.log("accountsCount", this.state.accounts.length);
-            console.log("accounts", this.state.accounts);
-            console.log("transactionsCount", this.state.transactions.length);
-          });
+        console.log("accountsCount", this.state.accounts.length);
+        console.log("accounts", this.state.accounts);
+        console.log("transactionsCount", this.state.transactions.length);
       });
+    });
     console.log("handleOnSuccess");
   }
 
